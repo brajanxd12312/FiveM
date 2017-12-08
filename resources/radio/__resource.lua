8f9b1bc38fec5475ae6ff16c04fa7a4260f09fdb
Sean <DevRain2500@users.noreply.github.com>
2017-12-08 12:01:06 -0500

    moar radios

--- resources/radio/__resource.lua
+++ resources/radio/__resource.lua
@@ -1,8 +1,9 @@
 resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
 
 -- Example custom radios
-supersede_radio "RADIO_02_POP" { url = "http://stream.radioreklama.bg/nrj.ogg", volume = 0.2, name = "Energy Radio" }
-supersede_radio "RADIO_07_DANCE_01" { url = "http://stream.radioreklama.bg/nova_low.ogg", volume = 0.2, name = "NOVA Radio" }
+supersede_radio "RADIO_02_POP" { url = "http://stream.radioreklama.bg/city.ogg", volume = 0.2, name = "Radio City" }
+supersede_radio "RADIO_07_DANCE_01" { url = "http://mp3.stream.tb-group.fm/tt.ogg", volume = 0.2, name = "24hr Dance" }
+supersede_radio "RADIO_04_PUNK" { url = "http://relay-nyc.gameowls.com:8000/ocremix.ogg", volume = 0.2, name = "wut..." }
 
 files {
 	"index.html"
