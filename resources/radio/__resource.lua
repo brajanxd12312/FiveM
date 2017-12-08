4bc83ed90dab68aed083b706d216a4ced141c0b8
Sean <DevRain2500@users.noreply.github.com>
2017-12-08 11:29:58 -0500

    new stations
    ```lua
    "RADIO_02_POP" { url = "http://stream.radioreklama.bg/nrj.ogg", volume = 0.2, name = "Energy Radio" }
    "RADIO_04_PUNK" { url = "http://stream.radioreklama.bg/nova_low.ogg", volume = 0.2, name = "NOVA Radio" }
    ```

--- resources/radio/__resource.lua
+++ resources/radio/__resource.lua
@@ -1,8 +1,8 @@
 resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
 
 -- Example custom radios
-supersede_radio "RADIO_02_POP" { url = "https://revolutionradio.ru:8443/live.ogg", volume = 0.2 }
-supersede_radio "RADIO_03_HIPHOP_NEW" { url = "http://stream.radioreklama.bg/nrj.ogg", volume = 0.2 }
+supersede_radio "RADIO_02_POP" { url = "http://stream.radioreklama.bg/nrj.ogg", volume = 0.2, name = "Energy Radio" }
+supersede_radio "RADIO_04_PUNK" { url = "http://stream.radioreklama.bg/nova_low.ogg", volume = 0.2, name = "NOVA Radio" }
 
 files {
 	"index.html"
