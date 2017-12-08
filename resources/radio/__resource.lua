b3d8ce00e2f9f694812de4af086fb172b217ad46
Sean <DevRain2500@users.noreply.github.com>
2017-12-08 11:12:33 -0500

    removed custom names
    for science

--- resources/radio/__resource.lua
+++ resources/radio/__resource.lua
@@ -1,8 +1,8 @@
 resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
 
 -- Example custom radios
-supersede_radio "RADIO_02_POP" { url = "https://revolutionradio.ru:8443/live.ogg", volume = 0.2 name = "Custom Radio 1" }
-supersede_radio "RADIO_03_HIPHOP_NEW" { url = "http://stream.radioreklama.bg/nrj.ogg", volume = 0.2 name = "Custom Radio 2" }
+supersede_radio "RADIO_02_POP" { url = "https://revolutionradio.ru:8443/live.ogg", volume = 0.2 }
+supersede_radio "RADIO_03_HIPHOP_NEW" { url = "http://stream.radioreklama.bg/nrj.ogg", volume = 0.2 }
 
 files {
 	"index.html"
