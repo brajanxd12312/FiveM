6fd0b05e2de786da7808b009c3c50b0bff31b52d
Sean <DevRain2500@users.noreply.github.com>
2018-07-17 05:14:38 -0400

    noCursor

--- resources/dev-scripts/client.lua
+++ resources/dev-scripts/client.lua
@@ -1,2 +1,42 @@
 -- Set weapon to 'hands' after the player is given them. Prevents spawning in with weapon in hand.
 SetCurrentPedWeapon(GetPlayerPed(-1), 'WEAPON_UNARMED', 1)
+
+--[[------------------------------------------------------------------------
+    Remove Reticle on ADS (Third Person)
+------------------------------------------------------------------------]]--
+local scopedWeapons = 
+{
+    100416529,  -- WEAPON_SNIPERRIFLE
+    205991906,  -- WEAPON_HEAVYSNIPER
+    3342088282  -- WEAPON_MARKSMANRIFLE
+}
+
+function HashInTable( hash )
+    for k, v in pairs( scopedWeapons ) do 
+        if ( hash == v ) then 
+            return true 
+        end 
+    end 
+
+    return false 
+end 
+
+function ManageReticle()
+    local ped = GetPlayerPed( -1 )
+
+    if ( DoesEntityExist( ped ) and not IsEntityDead( ped ) ) then
+        local _, hash = GetCurrentPedWeapon( ped, true )
+
+        if ( GetFollowPedCamViewMode() ~= 4 and IsPlayerFreeAiming() and not HashInTable( hash ) ) then 
+            HideHudComponentThisFrame( 14 )
+        end 
+    end 
+end 
+
+Citizen.CreateThread( function()
+    while true do 
+        ManageReticle()
+
+        Citizen.Wait( 0 )
+    end 
+end )
